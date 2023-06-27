e30aab444b5ed0c9e307141bde4f31264f805c80
gomljo <gomljo0307@naver.com>
2023-06-27 20:51:24 +0900

    test: Memo 엔티티 모든 데이터 조회 기능 테스트

--- src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
+++ src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -5,18 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import zerobase.weather.domain.Memo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 //@Transactional // 데이터베이스 테스트 시 많이 사용한다, 결과가 데이터베이스에 반영되지 않는다!
@@ -36,6 +34,7 @@ class JdbcMemoRepositoryTest {
                         .build()
                 );
         ArgumentCaptor<Memo> memoArgumentCaptor = ArgumentCaptor.forClass(Memo.class);
+
         // when
         jdbcMemoRepository.save(newMemo);
         // then
@@ -43,5 +42,38 @@ class JdbcMemoRepositoryTest {
         assertEquals(1, memoArgumentCaptor.getValue().getId());
         assertEquals("InsertMemoTest", memoArgumentCaptor.getValue().getText());
     }
+    private Memo createMemo(int id, String text){
+      return Memo.builder()
+              .id(id)
+              .text(text)
+              .build();
+    }
+    @Test
+    void findAllMemoTest() {
+        // given
+        Memo memoFirst = createMemo(1, "first memo");
+        Memo memoSecond = createMemo(2, "second memo");
+        Memo memoThird = createMemo(3, "third memo");
 
+        List<Memo> expectMemoList = new ArrayList<>();
+        expectMemoList.add(memoFirst);
+        expectMemoList.add(memoSecond);
+        expectMemoList.add(memoThird);
+
+        given(jdbcMemoRepository.findAll())
+                .willReturn(expectMemoList);
+        // when
+        List<Memo> actualMemoList = jdbcMemoRepository.findAll();
+        System.out.println(actualMemoList);
+
+        // then
+        assertEquals(3, actualMemoList.size());
+        assertEquals(1, actualMemoList.get(0).getId());
+        assertEquals("first memo", actualMemoList.get(0).getText());
+        assertEquals(2, actualMemoList.get(1).getId());
+        assertEquals("second memo", actualMemoList.get(1).getText());
+        assertEquals(3, actualMemoList.get(2).getId());
+        assertEquals("third memo", actualMemoList.get(2).getText());
+
+    }
 }
