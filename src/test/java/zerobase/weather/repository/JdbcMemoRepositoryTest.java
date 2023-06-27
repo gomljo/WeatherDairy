e0611378967b4d5e5745ef59e57d7e21904b84ac
gomljo <gomljo0307@naver.com>
2023-06-27 19:22:25 +0900

    refactor: Memo 엔티티 저장 기능 테스트 리팩터링
    모의 객체를 생성하여 데이터베이스에 실행 결과가 반영되지 않도록 리팩터링

--- src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
+++ src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -1,6 +1,10 @@
 package zerobase.weather.repository;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import zerobase.weather.domain.Memo;
@@ -8,23 +12,36 @@ import zerobase.weather.domain.Memo;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 //@Transactional // 데이터베이스 테스트 시 많이 사용한다, 결과가 데이터베이스에 반영되지 않는다!
 class JdbcMemoRepositoryTest {
 
-    @Autowired
+    @Mock
     JdbcMemoRepository jdbcMemoRepository;
 
     @Test
     void insertMemoTest() {
         // given
         Memo newMemo = new Memo(1, "InsertMemoTest");
+        given(jdbcMemoRepository.save(any()))
+                .willReturn(Memo.builder()
+                        .id(1)
+                        .text("InsertMemoTest")
+                        .build()
+                );
+        ArgumentCaptor<Memo> memoArgumentCaptor = ArgumentCaptor.forClass(Memo.class);
         // when
         jdbcMemoRepository.save(newMemo);
         // then
-        Optional<Memo> memo = jdbcMemoRepository.findById(1);
-        assertEquals(memo.get().getText(), "InsertMemoTest");
+        verify(jdbcMemoRepository, times(1)).save(memoArgumentCaptor.capture());
+        assertEquals(1, memoArgumentCaptor.getValue().getId());
+        assertEquals("InsertMemoTest", memoArgumentCaptor.getValue().getText());
     }
 
 }
