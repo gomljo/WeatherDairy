102689168187b5d9e5fcb19bad42c5b63a1489ed
gomljo <gomljo0307@naver.com>
2023-07-02 04:11:43 +0900

    feat: 날씨 데이터 저장 기능 구현

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -6,11 +6,16 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import zerobase.weather.config.OpenApiUriRequestConfig;
+import zerobase.weather.domain.VO.WeatherRecord;
+import zerobase.weather.repository.WeatherRecordRepository;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +25,23 @@ import static zerobase.weather.constant.JsonKey.*;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DiaryService {
+    private final WeatherRecordRepository weatherRecordRepository;
     private final OpenApiUriRequestConfig openApiUriRequestConfig;
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @Scheduled(cron = "0 0 1 * * *")
+    public void saveWeatherRecord(){
+        String weatherString = getWeatherString();
+        Map<String, Object> weatherMap = parseWeather(weatherString);
+
+        weatherRecordRepository.save(
+                WeatherRecord.builder()
+                        .date(LocalDate.now())
+                        .weather(weatherMap.get(MAIN.getJsonKey()).toString())
+                        .icon(weatherMap.get(ICON.getJsonKey()).toString())
+                        .temperature((Double) weatherMap.get(TEMP.getJsonKey()))
+                        .build());
+    }
 
     public String getWeatherString(){
         RestTemplate restTemplate = new RestTemplate();
